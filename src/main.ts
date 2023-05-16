88fde333068362ae66658e823d0cd395a029f914
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-05-16 15:38:14 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 // https://github.com/stackblitz/core/issues/2366
 import 'zone.js'; // Avoid error in StackBlitz
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withRouterConfig } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
-import { routes, config } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes, withRouterConfig(config))
-  ]
-});
+bootstrapApplication(AppComponent, appConfig);
